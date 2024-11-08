5c5e07d9718a2278e7745c0251b72a627af76fc8
mmm-maymyatmon <maymyat.dev@gmail.com>
2024-11-08 19:08:23 +0700

    Add Layout component with token authentication

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Header from './Header'
-import { Outlet } from 'react-router-dom'
+import { Outlet, Navigate } from 'react-router-dom'
 import toast, { Toaster } from 'react-hot-toast'
+import { useCookie } from 'react-use-cookie'
 
 const Layout = () => {
+  const [token] = useCookie("my_token");
+
+  if (!token) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <main className='flex flex-col min-h-screen p-5 '>
         <Header/>
